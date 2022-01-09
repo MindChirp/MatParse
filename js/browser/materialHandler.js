d8822a8bba79fe11a45fa80d6b3b62fdc2a6bda8
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2022-01-09 19:51:20 +0100

    fixed image bug

--- js/browser/materialHandler.js
+++ js/browser/materialHandler.js
@@ -157,11 +157,24 @@ function createMaterial(material) {
             }
         }
 
-        if(!_img) {reject(); return;};
 
-        var src = "data:image/png;base64," + _img.toString("base64");
-        
-        img.src = src;
+        var hasImage = true;
+        if(!_img) {
+            hasImage = false;
+        };
+
+        if(hasImage) {
+            var src = "data:image/png;base64," + _img.toString("base64");
+            
+            img.src = src;
+        } else {
+
+            var icon = document.createElement("div");
+            img.parentNode.replaceChild(icon, img);
+            icon.className = "no-image";
+
+            icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Image</title><rect x="48" y="80" width="416" height="352" rx="48" ry="48" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><circle cx="336" cy="176" r="32" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path d="M304 335.79l-90.66-90.49a32 32 0 00-43.87-1.3L48 352M224 432l123.34-123.34a32 32 0 0143.11-2L464 368" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>';
+        }
 
         parent.appendChild(el);
 
